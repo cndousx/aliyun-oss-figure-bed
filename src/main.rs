fa056666e74c360c4b72d1d3f9bde0d6df315dc2
DouShaoxun <dousx_cn@163.com>
2026-06-10 14:33:25 +0800

    0.1.13 fn

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,8 @@ use uuid::Uuid;
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    let is_md = &args[1] == "md";
-    let start = if is_md { 2 } else { 1 };
+    let (out_md, upload_infos) = parse_args(&args);
 
-    for i in start..args.len() {
-        let file = &args[i];
-        if !Path::new(&file).exists() {
-            panic!("文件 {} 不存在", file);
-        }
-        if let None = Path::new(&file).extension() {
-            panic!("文件 {} 没有扩展名", file);
-        }
-    }
     let client = Client::from_env().unwrap();
     let buckets = client.get_buckets().await.unwrap();
     if buckets.len() != 1 {
@@ -29,15 +19,9 @@ async fn main() {
     let bucket = buckets.get(0).unwrap();
     let bucket_url = bucket.to_url().unwrap().as_str().to_string();
 
-    let tasks: Vec<_> = (start..args.len())
-        .map(|i| {
-            let arg = &args[i];
-            let ext = match Path::new(arg).extension() {
-                None => {
-                    panic!();
-                }
-                Some(ext) => ext.to_string_lossy().to_string(),
-            };
+    let tasks = upload_infos
+        .iter()
+        .map(|(arg, ext)| {
             let timestamp = Local::now().format("%Y/%m/%d/%H-%M-%S-%3f").to_string();
             let uuid_simple = Uuid::new_v4().simple();
             let fs = format!("{timestamp}-{uuid_simple}.{ext}");
@@ -48,7 +32,7 @@ async fn main() {
                 match tokio::fs::File::open(arg).await {
                     Ok(file) => match &bucket.object(&md).upload(file).await {
                         Ok(_) => {
-                            if is_md {
+                            if out_md {
                                 Some(format!("![{timestamp}]({url})"))
                             } else {
                                 Some(url)
@@ -66,12 +50,9 @@ async fn main() {
                 }
             }
         })
-        .collect();
+        .collect::<Vec<_>>();
 
-    let max_concurrent = std::env::var("ALIYUN-OSS-FIGURE-BED-MAX-CONCURRENT")
-        .ok()
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(3); // 默认并发数为3
+    let max_concurrent = max_concurrent();
 
     // 使用 stream 控制并发数
     let results: Vec<_> = stream::iter(tasks)
@@ -85,4 +66,53 @@ async fn main() {
             None => {}
         }
     }
+}
+
+/// 获取最大并发数
+fn max_concurrent() -> usize {
+    std::env::var("ALIYUN-OSS-FIGURE-BED-MAX-CONCURRENT")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(1) as usize // 默认并发数为1
+}
+/// 解析参数
+fn parse_args(args: &Vec<String>) -> (bool, Vec<(&String, String)>) {
+    let (is_md, start) = validate_args(&args).unwrap();
+    (
+        is_md,
+        (start..args.len())
+            .map(|i| {
+                let arg = &args[i];
+                let file = Path::new(&arg);
+                if !file.exists() {
+                    panic!("文件不存在：{}", arg);
+                }
+                if let None = file.extension() {
+                    panic!("文件没有扩展名：{}", arg);
+                }
+                let ext = match file.extension() {
+                    None => {
+                        panic!("文件没有扩展名：{}", arg);
+                    }
+                    Some(ext) => ext.to_string_lossy().to_string(),
+                };
+                (arg, ext)
+            })
+            .collect::<Vec<_>>(),
+    )
+}
+/// 校验参数
+fn validate_args(args: &[String]) -> Result<(bool, usize), String> {
+    if args.len() < 2 {
+        return Err("至少需要一个文件参数".to_string());
+    }
+
+    let is_md = &args[1] == "md";
+    let start = if is_md { 2 } else { 1 };
+
+    if start >= args.len() {
+        return Err("没有提供文件参数".to_string());
+    }
+
+    Ok((is_md, start))
 }
